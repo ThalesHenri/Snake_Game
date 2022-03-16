4209114ac25478df03428aa50abdce1262890037
ThalesHenri <thales1337@hotmail.com>
2022-03-16 20:07:03 -0300

    erros corrigidos... Snake game versão beta do TH

--- controlador.py
+++ controlador.py
@@ -34,7 +34,6 @@ run = True
 # Game loop
 while run:
     clock.tick(10)  # our clock object will set the game fps
-    snake_obj.move()
     for event in pygame.event.get():#  event catcher for inputs
         if event.type == QUIT:
             pygame.quit()
@@ -48,15 +47,21 @@ while run:
             if event.key == K_RIGHT and snake_obj.snake_direction != snake_obj.LEFT:
                 snake_obj.snake_direction = snake_obj.RIGHT
 
-    if colision(snake_obj.snake_body[0], apple_obj.position):
+    if colision(snake_obj.snake_body[0], apple_obj.position):  # Snake eating
         apple_obj.onGrid_Random_Spawn()
         snake_obj.snake_body.append((0, 0))
 
-
+    snake_obj.move()
     SCREEN.fill(BLACK)
     snake_obj.snake_skin.fill(WHITE)
     apple_obj.apple.fill(RED)
+
     for pos in snake_obj.snake_body:
         SCREEN.blit(snake_obj.snake_skin, pos)
+
+    for f in range(1, len(snake_obj.snake_body)):
+        if colision(snake_obj.snake_body[0], snake_obj.snake_body[f]):
+            print('you died!')
+
     SCREEN.blit(apple_obj.apple, apple_obj.position)
     pygame.display.update()
